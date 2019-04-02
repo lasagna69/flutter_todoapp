2e256f53fbb590b87554b5beb9d1265d759c82ac
lasagna69 <tom.ortega47@gmail.com>
2019-04-02 16:32:46 +0800

    Tried to add a date picker and what the fuck is happening

--- lib/to_do_item.dart
+++ lib/to_do_item.dart
@@ -1,7 +1,8 @@
 class ToDoItem {
   final String title;
+  final DateTime date;
 
-  ToDoItem(this.title);
+  ToDoItem(this.title, this.date);
 
   @override
   bool operator ==(Object other) => identical(this, other) || other is ToDoItem && runtimeType == other.runtimeType && title == other.title;
--- lib/to_do_list_page.dart
+++ lib/to_do_list_page.dart
@@ -5,8 +5,34 @@ import 'package:redux/redux.dart';
 import 'package:flutter_todo/actions.dart';
 import 'package:flutter_todo/state.dart';
 import 'package:flutter_todo/to_do_item.dart';
+import 'package:datetime_picker_formfield/datetime_picker_formfield.dart';
+import 'package:intl/intl.dart';
 
 class ToDoListPage extends StatelessWidget {
+  Widget build(BuildContext context) {
+    return new MaterialApp(
+        title: 'Todo List',
+        home: new ToDoList()
+    );
+  }
+}
+
+class ToDoList extends StatefulWidget{
+  @override
+  createState() => new ToDoListState();
+}
+
+class ToDoListState extends State<ToDoList>{
+  final formats = {
+    InputType.both: DateFormat("EEEE, MMMM d, yyyy 'at' h:mma"),
+    InputType.date: DateFormat('yyyy-MM-dd'),
+    InputType.time: DateFormat("HH:mm"),
+  };
+
+  InputType inputType = InputType.both;
+  bool editable = true;
+  DateTime date;
+
   @override
   Widget build(BuildContext context) => StoreConnector<AppState, _ViewModel>(
     converter: (Store<AppState> store) => _ViewModel.create(store),
@@ -34,12 +60,32 @@ class ToDoListPage extends StatelessWidget {
   Widget _createEmptyItemWidget(_EmptyItemViewModel item) => Column(
     children: [
       TextField(
-        onSubmitted: item.onCreateItem,
-        autofocus: true,
+        //onSubmitted: item.onCreateItem,
+//        autofocus: true,
         decoration: InputDecoration(
           hintText: item.createItemToolTip,
         ),
-      )
+      ),
+      DateTimePickerFormField(
+        inputType: inputType,
+        format: formats[inputType],
+        editable: editable,
+        decoration: InputDecoration(
+            labelText: 'Date/Time', hasFloatingPlaceholder: false),
+        onChanged: (dt) => setState(() => date = dt),
+      ),
+      Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Builder(
+          builder: (context) {
+            return RaisedButton(
+              onPressed: () => submitTodo(item),
+              color: Colors.indigoAccent,
+              child: Text('Submit Todo'),
+            );
+          },
+        ),
+      ),
     ],
   );
 
@@ -55,6 +101,9 @@ class ToDoListPage extends StatelessWidget {
       )
     ],
   );
+  void submitTodo(_EmptyItemViewModel item) {
+    item.onCreateItem;
+  }
 }
 
 class _ViewModel {
@@ -75,15 +124,18 @@ class _ViewModel {
         .toList();
 
     if (store.state.listState == ListState.listWithNewItem) {
-      items.add(_EmptyItemViewModel('Type the next task here', (String title) {
+      items.add(_EmptyItemViewModel('Type the next task here', (String title, DateTime date) {
         store.dispatch(DisplayListOnlyAction());
-        store.dispatch(AddItemAction(ToDoItem(title)));
+        store.dispatch(AddItemAction(ToDoItem(title, date)));
         store.dispatch(SaveListAction());
       }, 'Add'));
     }
 
     return _ViewModel('To Do', items, () => store.dispatch(DisplayListWithNewItemAction()), 'Add new to-do item', Icons.add);
   }
+
+
+
 }
 
 abstract class _ItemViewModel {}
@@ -91,7 +143,7 @@ abstract class _ItemViewModel {}
 @immutable
 class _EmptyItemViewModel extends _ItemViewModel {
   final String hint;
-  final Function(String) onCreateItem;
+  final Function(String, DateTime) onCreateItem;
   final String createItemToolTip;
 
   _EmptyItemViewModel(this.hint, this.onCreateItem, this.createItemToolTip);
