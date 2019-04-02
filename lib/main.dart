f0bee46452cb0a7797540881660c5e27b5433ec1
lasagna69 <tom.ortega47@gmail.com>
2019-04-02 14:33:59 +0800

    trying redux on new branch

--- lib/main.dart
+++ lib/main.dart
@@ -1,186 +1,30 @@
 // Import MaterialApp and other widgets which we can use to quickly create a material app
 import 'package:flutter/material.dart';
-import 'package:shared_preferences/shared_preferences.dart';
-import 'package:flutter/services.dart';
+import 'package:redux/redux.dart';
+import 'package:flutter_redux/flutter_redux.dart';
+import 'package:flutter_todo/reducers.dart';
+import 'package:flutter_todo/middleware.dart';
+import 'package:flutter_todo/state.dart';
+import 'package:flutter_todo/to_do_list_page.dart';
 
-List<String> _todoList = [];
+void main() => runApp(ToDoListApp());
 
-// Code written in Dart starts exectuting from the main function. runApp is part of
-// Flutter, and requires the component which will be our app's container. In Flutter,
-// every component is known as a "widget".
-void main() => runApp(new TodoApp());
-
-// Every component in Flutter is a widget, even the whole app itself
-class TodoApp extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return new MaterialApp(
-        title: 'Todo List',
-        home: new TodoList()
-    );
-  }
-}
-
-class TodoList extends StatefulWidget {
-  @override
-  createState() => new TodoListState();
-}
-
-class TodoListState extends State<TodoList> {
-  List<String> _todoItems = [];
+class ToDoListApp extends StatelessWidget {
+  final Store<AppState> store = Store<AppState>(
+    appReducer,
+    initialState: AppState.initial(),
+    middleware: createStoreMiddleware(),
+  );
 
   @override
-  void initState(){
-    super.initState();
-//    const MethodChannel('plugins.flutter.io/shared_preferences')
-//        .setMockMethodCallHandler(
-//          (MethodCall methodcall) async {
-//        if (methodcall.method == 'getAll') {
-//          return {"flutter. [ No Name Saved ]"};
-//        }
-//        return null;
-//      },
-//    );
-    setData();
-//    this._todoItems = _todoList;
-//    print('inside initState: $_todoItems');
-  }
-
-  setData() {
-    SharedPreferencesTest.getList().then((value) {
-      setState(() {
-        _todoList = value;
-        _todoItems = value;
-      });
-    });
-  }
-
-  void _addTodoItem(String task) {
-    // Only add the task if the user actually entered something
-    if(task.length > 0) {
-      // Putting our code inside "setState" tells the app that our state has changed, and
-      // it will automatically re-render the list
-      setState(() => _todoItems.add(task));
-//      _todoList.add(task);
-    }
-  }
-
-  void _removeTodoItem(int index) {
-//    _todoList.removeAt(index);
-    setState(() => _todoItems.removeAt(index));
-  }
-
-  void _promptRemoveTodoItem(int index) {
-    showDialog(
-        context: context,
-        builder: (BuildContext context) {
-          return new AlertDialog(
-              title: new Text('Mark "${_todoItems[index]}" as done?'),
-              actions: <Widget>[
-                new FlatButton(
-                    child: new Text('CANCEL'),
-                    // The alert is actually part of the navigation stack, so to close it, we
-                    // need to pop it.
-                    onPressed: () => Navigator.of(context).pop()
-                ),
-                new FlatButton(
-                    child: new Text('MARK AS DONE'),
-                    onPressed: () {
-                      _removeTodoItem(index);
-                      SharedPreferencesTest.setList(_todoItems);
-                      Navigator.of(context).pop();
-                    }
-                )
-              ]
-          );
-        }
-    );
-  }
-
-  // Build the whole list of todo items
-  Widget _buildTodoList() {
-    return new ListView.builder(
-      itemBuilder: (context, index) {
-//        SharedPreferencesTest.getList();
-        setData();
-        _todoItems = _todoList;
-        // itemBuilder will be automatically be called as many times as it takes for the
-        // list to fill up its available space, which is most likely more than the
-        // number of items we have. So, we need to check the index is OK.
-        if(index < _todoItems.length) {
-          print("inside _todoItems: $_todoItems");
-          return _buildTodoItem(_todoItems[index], index);
-        }
-      },
-    );
-  }
-
-  // Build a single todo item
-  Widget _buildTodoItem(String todoText, int index) {
-    return new ListTile(
-        title: new Text(todoText),
-        onTap: () => _promptRemoveTodoItem(index)
-    );
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return new Scaffold(
-      appBar: new AppBar(
-          title: new Text('Todo List')
+  Widget build(BuildContext context) => StoreProvider(
+    store: this.store,
+    child: MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
       ),
-      body: _buildTodoList(),
-      floatingActionButton: new FloatingActionButton(
-          onPressed: _pushAddTodoScreen,
-          tooltip: 'Add task',
-          child: new Icon(Icons.add)
-      ),
-    );
-  }
-
-  void _pushAddTodoScreen() {
-    // Push this page onto the stack
-    Navigator.of(context).push(
-      // MaterialPageRoute will automatically animate the screen entry, as well as adding
-      // a back button to close it
-        new MaterialPageRoute(
-            builder: (context) {
-              return new Scaffold(
-                  appBar: new AppBar(
-                      title: new Text('Add a new task')
-                  ),
-                  body: new TextField(
-                    autofocus: true,
-                    onSubmitted: (val) {
-                      _addTodoItem(val);
-                      Navigator.pop(context); // Close the add todo screen
-                      SharedPreferencesTest.setList(_todoItems);
-                    },
-                    decoration: new InputDecoration(
-                        hintText: 'Enter something to do...',
-                        contentPadding: const EdgeInsets.all(16.0)
-                    ),
-                  )
-              );
-            }
-        )
-    );
-  }
-}
-
-class SharedPreferencesTest {
-  /// Method that returns the user todolist
-  static getList() async {
-    final SharedPreferences prefs = await SharedPreferences.getInstance();
-    List<String> temp = prefs.getStringList('todoList').toList();
-    _todoList = temp;
-    print("inside todoList sharedpreferences: $_todoList");
-    return temp;
-  }
-
-  /// Method that saves the user todolist
-  static setList(_todoItems) async {
-    final SharedPreferences prefs = await SharedPreferences.getInstance();
-    return prefs.setStringList('todoList', _todoItems);
-  }
+      home: ToDoListPage(),
+    ),
+  );
 }
